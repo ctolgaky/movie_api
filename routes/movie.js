5e3a729e6cb4be51772df06ab213c75a9bb777e1
Tolga <caglartolgakaya@hotmail.com>
2019-05-01 22:50:43 +0300

    add movie

--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+//Models
+const Movie = require('../models/Movie');
+
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
+  // const {title, imdb_score, category, country, year} = req.body;
+  //
+  // const movie = new Movie({
+  //   title:title,
+  //   imdb_score: imdb_score,
+  //   category: category,
+  //   country: country,
+  //   year: year
+  // });
+
+  // üst ksıım yerine sadece alttakini kullanabiliriz
+
+   const movie = new Movie(req.body);
+
+//   movie.save((err,data) =>{
+//     if(err)
+//       res.json(err);
+//     res.json({status: 1});
+//   })
+// });
+
+  const promise = movie.save();
+  promise.then((data) =>{
   res.json({status: 1});
+}).catch((err) => {
+  res.json(err);
+  })
 });
+
 
 module.exports = router;
