dd773eaaaa7db8abb1f89ae5695b49f4f7598cb8
Tolga <caglartolgakaya@hotmail.com>
2019-05-06 23:39:06 +0300

    without nodemon

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = () => {
   mongoose.connect('mongodb://ctolgaky:tolga123@ds147096.mlab.com:47096/movie_api',  { useNewUrlParser: true } )
   mongoose.connection.on('open', () =>{
-      console.log('MongoDB Connected')
+      //console.log('MongoDB Connected')
   })  ;
   mongoose.connection.on('error', (err) =>{
       console.log('MongoDB Error:', err)
--- test/movie.test.js
+++ test/movie.test.js
@@ -6,16 +6,131 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-describe('Node Server', ()=>{
-   it('(GET /) anasayfayı döndürür', (done) => {
-       chai.request(server)
-           .get('/')
-           .end((err,res)=>{
-               res.should.have.status(200);
-               done();
-           })
+let token, movieId;
 
-   }) ;
+describe('/api/movies test', ()=>{
 
 
+    before((done)=>{
+        chai.request(server)
+            .post('/authenticate')
+            .send({username: 'tolga', password:'tolga123'})
+            .end((err,res)=>{
+                token = res.body.token;
+                done();
+            });
+    });
+
+
+    describe('/GET movies', ()=>{
+        it('it should GET all the movies', (done)=>{
+            chai.request(server)
+                .get('/api/movies')
+                .set('x-access-token', token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+
+    });
+
+    describe('/POST movie',()=>{
+       it('it should POST a movie', (done)=>{
+           const movie = {
+               title: 'Salako',
+               director_id: '5ccb47225b5b051e9c2c8f5a',
+               category: 'Comedy',
+               country: 'Turkey',
+               year: 1974,
+               imdb_score: 8.2
+           };
+
+           chai.request(server)
+               .post('/api/movies')
+               .send(movie)
+               .set('x-access-token',token)
+               .end((err,res)=>{
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('title');
+                  res.body.should.have.property('director_id');
+                  res.body.should.have.property('category');
+                  res.body.should.have.property('country');
+                  res.body.should.have.property('year');
+                  res.body.should.have.property('imdb_score');
+                  movieId = res.body._id;
+                  done();
+               });
+       }) ;
+    });
+
+    describe('/GET/:movie_id movie', ()=>{
+        it('it should GET a movie by the given id', (done)=>{
+            chai.request(server)
+                .get('/api/movies/'+ movieId)
+                .set('x-access-token', token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movieId);
+                    done();
+
+                });
+        });
+    })
+
+    describe('/PUT/:movie_id movie',()=>{
+        it('it should UPDATE a movie given by id', (done)=>{
+            const movie = {
+                title: 'yeniFilm',
+                director_id: '5ccb47225b5b051e9c2c8f5b',
+                category: 'Crime',
+                country: 'France',
+                year: 1974,
+                imdb_score: 8.2
+            };
+
+            chai.request(server)
+                .put('/api/movies/'+ movieId)
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category)
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+
+                    done();
+                });
+        }) ;
+    });
+
+    describe('/DELETE/:movie_id movie',()=>{
+        it('it should DELETE a movie given by id', (done)=>{
+
+
+            chai.request(server)
+                .delete('/api/movies/'+ movieId)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+
+
+                    done();
+                });
+        }) ;
+    });
 });
